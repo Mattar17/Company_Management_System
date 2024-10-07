3e299cb17cb01ba6a493523618506352b63d26fb
mattar17 <mattar17x@gmail.com>
2024-10-07 23:21:42 +0300

    fixed login bug

--- Demo.BusinessLogicLayer/obj/Debug/net5.0/Demo.BusinessLogicLayer.AssemblyInfo.cs
+++ Demo.BusinessLogicLayer/obj/Debug/net5.0/Demo.BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3945472b214d6061b0d11b93697571e5236446")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
